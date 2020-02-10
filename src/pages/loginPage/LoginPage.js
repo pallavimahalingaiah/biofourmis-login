d2aa5e9b3e56c28b0756b52ffccfc778d4466ccc
Pallavi Mahalingaiah <pallavi.mahalingaiah@Honeywell.com>
2020-02-10 15:08:14 +0530

    media queries for modal responsiveness

--- src/pages/loginPage/LoginPage.js
+++ src/pages/loginPage/LoginPage.js
@@ -22,70 +22,76 @@ class LoginPage extends Component {
   render() {
     return (
       <div className="layout-image">
-        <Modal
-          show={true}
-          aria-labelledby="contained-modal-title-vcenter"
-          animation={false}
-          backdrop={false}
-          dialogClassName="modal-40w"
-        >
-          <Modal.Header className="loginpage-logo">
-            <Modal.Title id="contained-modal-title-vcenter">
-              <Container>
-                <Row>
-                  <Col xs={12}>
-                    <Image src={logo} alt="logo" />
-                  </Col>
-                </Row>
-              </Container>
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Header className="loginpage-login-tab">
-            <Container fluid>
-              <Row>
-                {this.state.showForgotPassword ? (
-                  <Col xs style={{ textAlign: "center" }}>
-                    <Button
-                      active
-                      variant="link"
-                      className="loginpage-forgot-Password-tab-btn"
-                    >
-                      Create new password
-                    </Button>
-                  </Col>
-                ) : (
-                  <>
-                    <Col xs={5}>
-                      <Button
-                        active
-                        variant="link"
-                        className="loginpage-login-tab-btn"
-                      >
-                        Login
-                      </Button>
-                    </Col>
+        <Container>
+          <Row>
+            <Col xs>
+              <Modal
+                show={true}
+                aria-labelledby="contained-modal-title-vcenter"
+                animation={false}
+                backdrop={false}
+                dialogClassName="modal-width"
+              >
+                <Modal.Header className="loginpage-logo">
+                  <Modal.Title id="contained-modal-title-vcenter">
+                    <Container fluid>
+                      <Row>
+                        <Col xs={12}>
+                          <Image src={logo} alt="logo" />
+                        </Col>
+                      </Row>
+                    </Container>
+                  </Modal.Title>
+                </Modal.Header>
+                <Modal.Header className="loginpage-login-tab">
+                  <Container fluid>
+                    <Row>
+                      {this.state.showForgotPassword ? (
+                        <Col xs style={{ textAlign: "center" }}>
+                          <Button
+                            active
+                            variant="link"
+                            className="loginpage-forgot-Password-tab-btn"
+                          >
+                            Create new password
+                          </Button>
+                        </Col>
+                      ) : (
+                        <>
+                          <Col xs={5}>
+                            <Button
+                              active
+                              variant="link"
+                              className="loginpage-login-tab-btn"
+                            >
+                              Login
+                            </Button>
+                          </Col>
 
-                    <Col className="loginpage-forgot-password ">
-                      <Button variant="link" onClick={this.handleFogotPassword}>
-                        Forgot Password?
-                      </Button>
-                    </Col>
-                  </>
-                )}
-              </Row>
-            </Container>
-          </Modal.Header>
-          <Modal.Body className="loginpage-modal-body">
-            {this.state.showForgotPassword ? (
-              <ForgotPasswordForm />
-            ) : (
-              <LoginForm></LoginForm>
-            )}
-          </Modal.Body>
-          {/* <Modal.Footer className="loginpage-login-btn">
-            <Button>Login</Button>
-          </Modal.Footer> */}
-        </Modal>
+                          <Col className="loginpage-forgot-password ">
+                            <Button
+                              variant="link"
+                              onClick={this.handleFogotPassword}
+                            >
+                              Forgot Password?
+                            </Button>
+                          </Col>
+                        </>
+                      )}
+                    </Row>
+                  </Container>
+                </Modal.Header>
+                <Modal.Body className="loginpage-modal-body">
+                  {this.state.showForgotPassword ? (
+                    <ForgotPasswordForm />
+                  ) : (
+                    <LoginForm></LoginForm>
+                  )}
+                </Modal.Body>
+              </Modal>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
